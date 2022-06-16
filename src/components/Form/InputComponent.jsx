3c6cfd48585f23b1fa3da263dfe6de96b511f0f7
Adrian Rangel <adrijar21@gmail.com>
2022-06-16 00:11:41 -0400

    form completed

--- src/components/Form/InputComponent.jsx
+++ src/components/Form/InputComponent.jsx
@@ -6,7 +6,7 @@ import './form.css'
 
 export function InputComponent() {
 
-  const {handleSubmit, register, formState: {errors, isSubmitting}, } = useForm()
+  const {handleSubmit, register, formState: {errors, isSubmitting}, } = useForm();
 
   function onSubmit(values) {
     return new Promise((resolve) => {
@@ -19,78 +19,74 @@ export function InputComponent() {
 
 
   return (
-    <Box mt={4} p={8} bgColor="white" borderRadius="8px" display="flex" flexDirection="column" alignItems="center">
+    <Box position="relative" mt={4} p={8} bgColor="white" borderRadius="8px" display="flex" flexDirection="column" alignItems="center">
       
       <form className='form' onSubmit={handleSubmit(onSubmit)}>
 
-        <FormControl isInvalid={errors.name}>
+        <FormControl isInvalid={errors.firstname}>
           <Input 
-          id="username" 
           name="firstname" 
           type="username" 
           focusBorderColor="purple.600" 
-          mb={6} 
+          mb={10} 
           size="lg" 
           placeholder="First Name" 
           _placeholder={ {fontWeight:'semibold', fontSize:15} }
           {...register('firstname', {
-            required:'This is required', 
-            minLength: {value: 4, message:'Minimum Length should be 4 characters'}
+            required: true, 
+            minLength: {value: 4, message:'First Name cannot be empty'}
           })}
         />
-        <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
+        <FormErrorMessage fontSize="12px" fontStyle="italic" position="absolute" top="45px" left="17rem" fontWeight="bold">{errors.firstname && errors.firstname.message}</FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.name}>
+        <FormControl isInvalid={errors.lastname}>
           <Input 
-          id="secondname" 
           name="lastname" 
           type="username" 
           focusBorderColor="purple.600" 
-          mb={6} 
+          mb={10} 
           size="lg" 
           placeholder="Last Name" 
           _placeholder={ {fontWeight:'semibold', fontSize:15} }
           {...register('lastname', {
-            required:'This is required', minLength: {value: 4, message:'Minimum Length should be 4 characters'}
+            required: true, minLength: {value: 4, message:'Last Name cannot be empty'}
           })}
           />
-          <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
+          <FormErrorMessage fontSize="12px" fontStyle="italic" position="absolute" top="45px" left="17rem" fontWeight="bold">{errors.lastname && errors.lastname.message}</FormErrorMessage>
         </FormControl>
     
         <FormControl isInvalid={errors.email}>
         <Input 
-          id="email" 
           name="email" 
           type="email" 
           focusBorderColor="purple.600" 
-          mb={6} 
+          mb={10} 
           size="lg" 
           placeholder="Email Address" 
           _placeholder={ {fontWeight:'semibold', fontSize:15} }
-          {...register('email', {required:'This is required',  pattern: /^\S+@\S+$/i})}
+          {...register('email', {required: true, message: 'Looks like this is not a email',  pattern: /^\S+@\S+$/i})}
           />
-          <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
+          <FormErrorMessage fontSize="12px" fontStyle="italic" position="absolute" top="45px" left="17rem" fontWeight="bold">{errors.email && errors.email.message}</FormErrorMessage>
         </FormControl>
       
 
 
         <FormControl isInvalid={errors.password}>
           <Input 
-          id="pswd" 
           name="password" 
           type="password" 
           focusBorderColor="purple.600" 
-          mb={6} 
+          mb={10}
           size="lg" 
           placeholder="Password" 
           _placeholder={ {fontWeight:'semibold', fontSize:15} }
           {...register('password', {
-            required : 'This is required', 
-            minLength : {value: 6, message:'The password must be contain should be 4 characters'}
+            required : true, 
+            minLength : {value: 6, message:'Password cannot be empty'}
           })}
           />
-          <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
+          <FormErrorMessage fontSize="12px" fontStyle="italic" position="absolute" top="45px" left="17rem" fontWeight="bold">{errors.password && errors.password.message}</FormErrorMessage>
         </FormControl>
       
         <Submit isLoading={isSubmitting} />
